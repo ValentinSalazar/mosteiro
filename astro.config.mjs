fdc9f29768c1ebdd1db417bbf026dbae33d51f2a
valentinsalazar <facundev@outlook.com>
2024-10-25 06:49:28 -0300

    configurando adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [tailwind()]
 });
