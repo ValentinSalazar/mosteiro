92d9e964062ff4c8ec8f515f3638ff91ce876a32
valentinsalazar <facundev@outlook.com>
2024-10-25 06:53:59 -0300

    configurando adapter correctamente xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
