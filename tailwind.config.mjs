d410e18ae70758afd878112beedeb364a9ff0658
valentinsalazar <facundev@outlook.com>
2024-11-08 15:50:05 -0300

    agregando el navbar mobile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,36 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-			'xs': '360px', // Celulares pantalla muy chica
-			'sm': '400px', // Celulares
-			'md': '768px', // Celulares Horizontal y Tablet Vertical
-			'lg': '1024px', // Tablet Horizontal y Laptops
-			'xl': '1280px', // Laptops y escritorios chicos
-			'2xl': '1536px', // Escritorios grandes
-		},
-		extend: {
-			fontFamily: {
-				sans: ['Poppins', 'sans-serif'],
-			},
-			colors: {
-				'firstColor': '#151515',
-				'secondColor': '#EADFCA',
-			},
-			boxShadow: {
-				'leftShadow': '-6px 0px 8px rgba(0, 0, 0, 0.3)',
-			},
-			backgroundImage: {
-				'mainImage': "url('/home.webp')",
-				'cocinaImage': "url('/cocinaBkg.webp')",
-				'proceso-1': 'url("/proceso-1.png")',
-				'proceso-2': 'url("/proceso-2.png")',
-				'garantia': 'url("/imageGarantia.webp")',
-				'nosotrosLeft': 'url("/nosotros-left.webp")',
-				'nosotrosRight': 'url("/nosotros-right.webp")',
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      'xs': '360px', // Celulares pantalla muy chica
+      'sm': '400px', // Celulares
+      'md': '768px', // Celulares Horizontal y Tablet Vertical
+      'lg': '1024px', // Tablet Horizontal y Laptops
+      'xl': '1280px', // Laptops y escritorios chicos
+      '2xl': '1536px', // Escritorios grandes
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'firstColor': '#151515',
+        'secondColor': '#EADFCA',
+        'thirdColor': '#B91111',
+      },
+      boxShadow: {
+        'leftShadow': '-6px 0px 8px rgba(0, 0, 0, 0.3)',
+      },
+      backgroundImage: {
+        'mainImage': "url('/home.webp')",
+        'cocinaImage': "url('/cocinaBkg.webp')",
+        'proceso-1': 'url("/proceso-1.png")',
+        'proceso-2': 'url("/proceso-2.png")',
+        'garantia': 'url("/imageGarantia.webp")',
+        'nosotrosLeft': 'url("/nosotros-left.webp")',
+        'nosotrosRight': 'url("/nosotros-right.webp")',
+      }
+    },
+  },
+  plugins: [],
 }
